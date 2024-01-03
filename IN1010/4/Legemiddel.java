df54a8286467070a3aee3ebfd187700250be8cd8
Evgeny Pivovarskiy <officialdeltain@gmail.com>
2024-01-03 18:47:58 +0100

    Moved Legemiddel ID generation to Legemiddel class

--- IN1010/4/Legemiddel.java
+++ IN1010/4/Legemiddel.java
@@ -2,11 +2,17 @@ abstract class Legemiddel{
 	public final String navn;
 	public int pris;
 	public final double virkestoff; // 
+	
+	private static int nesteid;
+	public final int id;
 
 	public Legemiddel(String navn, int pris, double virkestoff){
 		this.navn = navn;
 		this.pris = pris;
 		this.virkestoff = virkestoff;
+
+		nesteid++;
+		id = nesteid-1;
 	}
 
 	public String hentNavn(){
@@ -19,6 +25,10 @@ abstract class Legemiddel{
 
 	public void settNyPris(int nyPris){
 		this.pris = nyPris;
+	}
+
+	public int hentId(){
+		return this.id;
 	}
 
 }
--- IN1010/4/Vanlig.java
+++ IN1010/4/Vanlig.java
@@ -1,11 +1,7 @@
 public class Vanlig extends Legemiddel{
-	private static int nesteid = 0;
-	public final int id;
 
 	public Vanlig(String navn, int pris, double virkestoff){
 		super(navn, pris, virkestoff);
-		nesteid++;
-		id = nesteid-1;
 	}
 
 }
